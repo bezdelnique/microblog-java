3d392a035f7ddee48fcb39dd63d0b979287b2829
Vitaly Belenky <vitaly.belenky@gmail.com>
2020-12-06 14:14:26 +0300

    CRUD для поста

--- src/main/java/com/heman/blog/entities/PostRepo.java
+++ src/main/java/com/heman/blog/entities/PostRepo.java
@@ -1,10 +1,6 @@
 package com.heman.blog.entities;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 public interface PostRepo extends JpaRepository<Post, Integer> {
 
